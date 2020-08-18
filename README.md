3aa48c325881af0f11f4b96177c9af4f273e33d5
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2020-08-18 18:45:34 +0530

    added dart file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Flutter-by-Geekyamitesh
+# This is repo for students who mentor by @[GeekyAmitesh](https://github.com/geekyamitesh)
+
+
+
+
+
+### Show some :heart: and :star: the repo to support the project. 
+For more, contact me @[facebook](https://www.facebook.com/amiteshmani.tiwari), @[Twitter](https://twitter.com/amitesh_mani) or @[Linkedin](https://www.linkedin.com/in/amitesh-mani-tiwari)
+
+
+
+## Resources
+I have added practce file name practice.dart
+@[dart](https://dart.dev/guides)
+@[dart-online-compiler](https://dartpad.dev/)
+@[flutter](https://flutter.dev/docs)
+ 
+### Project will base on flutter , flutter api
+   
+
+## Facing Any Problem or need any Help:grey_question:
+Write me in [issues]((https://github.com/geekyamitesh) section. I will try solve your issue within 10-12 hours.
+
+ 
+
---
+++ practice.dart
@@ -0,0 +1,154 @@
+/*
+// String
+var num = 'amit';
+//rawstring
+var num1 = r'I am super hero';
+// String interpulation
+*/
+/*
+void main() {
+  var age = 30;
+  var str = "My age is $age";//string interpulation 
+  print(str);
+}
+*/
+// Type cob=version in dart
+// parse & assert
+//string into int
+/*
+void main() {
+  String p1 = 3.145889998878.toStringAsFixed(2);
+  print(p1);
+}
+*/
+/*
+void main() {
+  // For loop (intilial, condition, expression)
+  print('Flutter Class');
+  for (int c1 = 1; c1 <= 5; c1++) {
+    print(c1);
+  }
+
+
+
+    var ctr =1;
+    var maxCtr =5;
+    print("hello ravi");
+    //
+    while(ctr<=maxCtr){      
+        print(ctr);
+        ctr = ctr+1;
+    }
+
+
+
+    var ct =1;
+    var max1 =5;
+    
+    do{      
+        print("Hello World! Value is :${ct}");  
+        ct = ct+1;
+    }while(ct<=max1);
+
+}
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter number in string: ");
+  var numb = stdin.readLineSync();
+  var Fnumb = double.parse(numb);
+  stdout.write("String to float is $Fnumb");
+
+  print(Fnumb);
+}
+
+*/
+/*
+Type Casting Examples
+1.String to int
+2.String to double
+3.int to String
+4.double to String
+import 'dart:io';
+
+main() {
+// String to int
+  var one = int.parse('7');
+  assert(one == 7);
+  print(one);
+
+  // String to double
+  var onePointOne = double.parse('1.67');
+  print(onePointOne); 
+
+  // int to String
+  String x = 20.toString();
+  print(x);
+
+  // double to String
+  String pi = 3.14316.toStringAsFixed(2);
+  print(pi);
+  // importing dart:io file
+
+  print("Enter your name?");
+  // Reading name of the Geek
+  String name = stdin.readLineSync();
+
+  // Printing the name
+  print("Hello, $name! \nWelcome to GirlScript Foundation!!");
+}
+*/
+
+//Function in a dart
+// Dart funtionsyntax with parameters
+/*
+ return_type fun_nam(parmeter_list){
+   //statment(s)
+   retun value:
+ }
+
+ int square(int n1){
+   int s;
+   s= n1*n1;
+   return(s);
+ }
+
+ // Calling funtion in Dart
+ normal syntax
+ fun_nam(<argument_list>);
+
+ square(10);
+
+
+void main() {
+  int s = square(10);
+  //calling function square
+  print(s);
+}
+
+int square(int s) {
+  return s * s;
+}
+*/
+
+// Dart Anonymous
+
+// In a dart , We have seen many function that have names with full syntax. But we can also create namelesss function known as anonymouys e.g Lambda and closure
+/*
+
+1. An anonmoys function can have zero or more parameters
+2. We can also put constant or variable .
+
+syntax:-
+(parameter-list){
+  statement.....
+}
+
+*/
+void main() {
+  var list = ['amit', 10, 10.34];
+//untyped parameter ,item,the function ,invoked for each item in the list ,print a tring the values at the specific position
+  list.forEach((item) {
+    print('${list.indexOf(item)}: $item');
+  });
+}
